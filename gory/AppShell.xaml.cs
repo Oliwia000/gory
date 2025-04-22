78948f140c521f9e5db3ca6d1afed54adeec6d0e
teren <teren@KOMP>
2025-04-22 15:26:48 +0200

    Dodaj pliki projektów.

---
+++ gory/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace gory
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ gory/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace gory
+{
+    /// <summary>
+    /// Główna strona aplikacji-zarządza licznikiem polubień i  zapisuje dane.
+    /// </summary>
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Obsługuje  przycisk "Polub"-zwiększa liczbę polubień o 1.
+        /// </summary>
+        /// <param name="sender">Obiekt, który wywołuje zdarzenie</param>
+        /// <param name="e">Dane zdarzenia.</param>
+        private void OnIncrement_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            CounterBtn.Text = $"{count} polubień";
+        }
+        /// <summary>
+        /// Obsługuje  przycisk "Usuń"-zmiejsza liczbę polubień o 1,ale tylko jeśli liczba polubień jest większa od 0.
+        /// </summary>
+        /// <param name="sender">Obiekt, który wywołuje zdarzenie</param>
+        /// <param name="e">Dane zdarzenia.</param>
+        private void OnDecrement_Clicked(object sender, EventArgs e)
+        {
+            if (count > 0)
+                count--;
+
+            CounterBtn.Text = $"{count} polubień";
+        }
+        /// <summary>
+        /// Obsługuje przycisk "Zapisz", wyświetlając komunikat o zapisaniu danych.
+        /// </summary>
+        /// <param name="sender">Obiekt, który wywołuje zdarzenie</param>
+        /// <param name="e">Dane zdarzenia.</param>
+        private async void OnSave_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Zapisano", "Zapisano dane o domku!", "OK");
+        }
+    }
+}
